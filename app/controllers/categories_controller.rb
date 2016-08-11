d4fd6fb99f08184730404f9ea3d26ac236185e9b
Marshall Gray <marshallgray1992@gmail.com>
2016-08-11 17:02:20 +1000

    Update all views with categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -20,7 +20,8 @@ class CategoriesController < ApplicationController
   end
 
   def show
-
+    @category = Category.find(params[:id])
+    @category_articles = @category.articles.paginate(page: params[:page], per_page: 5)
   end
 
 # anything put in private will only be handled in that controller
